8201d6ed7372849286a9367d17a9377a6640d5f6
Fabio Ferreira <fabioyferreira>
2026-06-28 20:38:41 +0000

    enunciado da questao2

---
+++ atividade2OperacoesAritmaticasBasicas.py
@@ -0,0 +1,7 @@
+'''
+Atividade 2 - Operações Aritméticas Básicas
+Enunciado: Faça um algoritmo em Python que leia dois números reais. O algoritmo deverá
+somar os dois números e exibir o resultado final na tela.
+
+'''
+
