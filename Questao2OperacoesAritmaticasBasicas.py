9113c832a762be6272098b302969e427058b9402
Fabio Ferreira <fabioyferreira>
2026-06-28 20:50:51 +0000

    questao 2 resolvida

--- Questao2OperacoesAritmaticasBasicas.py
+++ Questao2OperacoesAritmaticasBasicas.py
@@ -4,3 +4,11 @@ Enunciado: Faça um algoritmo em Python que leia dois números reais. O algoritm
 somar os dois números e exibir o resultado final na tela.
 
 '''
+
+numero1 = float(input("Digite o primeiro numero"))
+numero2 = float(input("Digite o segundo numero"))
+
+
+soma = numero1 +numero2
+
+print(f"A soma dos dois numeros eh: {soma}")
